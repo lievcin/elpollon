3d1eb32ef2f55f2717da9472053044ee3d22e074
Liev <liev.garcia@gmail.com>
2014-01-04 11:39:45 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elpollon::Application.config.secret_key_base = 'a70379213876c2f812320cb9769665c4dd42b49c12e83f8a93352eecd70f8403751274782de144d75c244c100bf58600f2371b1753e51f74e3523d985b3ac175'
