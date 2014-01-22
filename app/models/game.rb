74bb20394a040a5a44d7151f67c76c8f78488f6c
Liev <liev.garcia@gmail.com>
2014-01-22 19:07:36 +0000

    points allocation logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -29,31 +29,45 @@ class Game < ActiveRecord::Base
   	
    	def assign_points 	  
    	  self.bets.each do |bet|
- 				p = points_per_game(self.home_score, self.away_score, bet.home_bet, bet.away_bet)
- 				bet.total_points = p
- 				bet.save
+
+ 				bet.total_points = [ full_points(bet.home_bet, bet.away_bet), 
+                             winner_points(bet.home_bet, bet.away_bet) +
+                             goal_diff_points(bet.home_bet, bet.away_bet) ].max + bonus_points(bet.home_bet, bet.away_bet)          
+ 				bet.save!
  	    end
     end
 
-  	def points_per_game(home_score, away_score, home_bet, away_bet)
+  	def full_points(home_bet, away_bet)
   		if home_score == home_bet && away_score == away_bet
-  			return 3
+  			4
   		else
-  			if (home_score - away_score) - (home_bet - away_bet) == 0 
-  				if home_score == away_score 
-  					return 1
-  				else
-  					return 2
-  				end
-  			else
-  				if ((home_score - away_score) <=> 0) == ((home_bet - away_bet) <=> 0) 
-  					return 1
-  				else
-  					return 0
-  				end
-  			end
+  			0
   		end
   	end
-  		
+
+  	def winner_points(home_bet, away_bet)
+  		if ((home_score - away_score) <=> 0) == ((home_bet - away_bet) <=> 0)
+  			1
+  		else
+  			0
+  		end
+  	end
+
+  	def goal_diff_points(home_bet, away_bet)
+  		if (home_score - away_score) == (home_bet - away_bet)
+  			1
+  		else
+  			0
+  		end
+  	end
+
+  	def bonus_points(home_bet, away_bet)  	  
+  		if (goal_diff_points(home_bet, away_bet) == 4 || goal_diff_points(home_bet, away_bet) == 1) && (home_score + away_score >= 5)  		
+  			1
+  		else
+  			0
+  		end
+  	end  		
+
 end
 
