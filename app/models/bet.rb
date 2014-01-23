dfd1ccfb4f4a4a9d16a7a0ba664998a4504cfff2
Liev <liev.garcia@gmail.com>
2014-01-23 18:18:39 -0500

    remove uniqueness of bet for user

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -13,6 +13,5 @@ class Bet < ActiveRecord::Base
   validates_numericality_of :home_bet, :numericality => { :greater_than_or_equal_to => 0, :only_integer => true }
   validates :away_bet,		:presence => true
   validates_numericality_of :away_bet, :numericality => { :greater_than_or_equal_to => 0, :only_integer => true }
-  validates_uniqueness_of		:game_id, :scope => :user_id
   	
 end
